7bf879892574e47bfb49b138fb9491380af484bb
Tomasz Knura <tomasz.knura@itsilesia.com>
2021-08-26 22:22:06 +0200

    refactor: decomposed FriendsScreen (#9)

--- src/screens/FriendsScreen.tsx
+++ src/screens/FriendsScreen.tsx
@@ -1,95 +1,39 @@
 import { useTranslation } from 'react-i18next'
 import {
   Accordion,
-  AccordionButton,
-  AccordionIcon,
   AccordionItem,
   AccordionPanel,
-  Badge,
   Box,
-  Button,
-  Flex,
-  Heading,
-  HStack,
-  IconButton,
-  Spacer,
   Text
 } from '@chakra-ui/react'
-import { FaArrowLeft, FaCheck, FaTimes } from 'react-icons/fa'
 
 import { RouteContainer } from 'components/navigation/RouteContainer'
-import { useHistory } from 'react-router'
-import { TextField } from 'components/ui/TextField'
+import PendingFriendsList from 'components/data/friends/PendingFriendsList'
+import { AllFriendsList } from 'components/data/friends/AllFriendsList'
+import { AccordionHeading } from 'components/ui/AccordionHeading'
+import { BackButton } from 'components/common/BackButton'
+import { AddFriendField } from 'components/data/friends/AddFriendField'
 
-const FriendsScreen = () => {
+const FriendsScreen = (): JSX.Element => {
   const { t } = useTranslation()
-  const history = useHistory()
 
   return (
     <RouteContainer>
       <Box w="100%" p={5}>
-        <Button leftIcon={<FaArrowLeft />} onClick={() => history.goBack()}>
-          {t('common.back')}
-        </Button>
-        <Text fontSize="6xl">
-          {t('common.friends')}
-        </Text>
-        <Flex alignItems="flex-end">
-          <TextField label={t('screens.friends.friendUsername')} />
-          <Button colorScheme="primary" ml={5}>
-            Add Friend
-          </Button>
-        </Flex>
+        <BackButton />
+        <Text fontSize="6xl">{t('common.friends')}</Text>
+        <AddFriendField />
         <Accordion mt={5} defaultIndex={[0, 1]} allowMultiple>
           <AccordionItem>
-            <AccordionButton>
-              <Flex>
-                <Heading fontSize="md">
-                  {t('screens.friends.pendingList')}
-                </Heading>
-              </Flex>
-              <Spacer />
-              <AccordionIcon />
-            </AccordionButton>
+            <AccordionHeading heading={t('screens.friends.pendingList')} />
             <AccordionPanel>
-              <HStack spacing={2}>
-                <Text>
-                  {t('screens.friends.list')}
-                </Text>
-                <IconButton
-                  size="sm"
-                  colorScheme="green"
-                  aria-label="accept"
-                  icon={<FaCheck />}
-                />
-                <IconButton
-                  size="sm"
-                  colorScheme="red"
-                  aria-label="decline"
-                  icon={<FaTimes />}
-                />
-              </HStack>
+              <PendingFriendsList />
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem>
-            <AccordionButton>
-              <Flex>
-                <Heading fontSize="md">
-                  {t('screens.friends.list')}
-                </Heading>
-              </Flex>
-              <Spacer />
-              <AccordionIcon />
-            </AccordionButton>
+            <AccordionHeading heading={t('screens.friends.list')} />
             <AccordionPanel>
-              <HStack>
-                <Text>
-                  XD
-                </Text>
-                <Badge ml={2} colorScheme="primary">
-                  {t('screens.friends.pending')}
-                </Badge>
-              </HStack>
+              <AllFriendsList />
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
